d29f4c5e75dbc2c1b898bceb12577c03194fdb41
Alex <alexey-bycov@yandex.ru>
2019-03-01 12:03:12 +0700

    Create GGA class

---
+++ GPSManager/GGA.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPSManager
+{
+    class GGA
+    {
+        private enum CoordinateKind { Latitude, Longitude }
+
+        private const int GGACommaPartsNum = 15;
+
+        public bool IsDetermined { get; private set; }
+        public readonly double Latitude;
+        public readonly double Longitude;
+        
+        public GGA()
+        {
+            Latitude = 0;
+            Longitude = 0;
+        }
+
+        public GGA(double Latitude, double Longitude)
+        {
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            IsDetermined = true;
+        }
+
+        public static GGA Parse(string s)
+        {
+            if(!TryParse(s, out var gga))
+            {
+                throw new ArgumentException($"Failed to parse GGA '{s}'");
+            }
+            return gga;
+        }
+
+        public static bool TryParse(string s, out GGA gga)
+        {
+            if (!s.StartsWith("$GPGGA") && !s.StartsWith("$GNGGA") ||
+                !SplitChecksum(s, out var mainPart, out var checksumPart) ||
+                !ValidateChecksum(mainPart, checksumPart) ||
+                !SplitMainPart(s, out var subparts))
+            {
+                gga = default(GGA);
+                return false;
+            }
+
+            string positionFixIndicator = subparts[6];
+            if(positionFixIndicator == "0")
+            {
+                gga = new GGA { IsDetermined = false };
+                return true;
+            }
+
+            double latitude, longitude;
+            try
+            {
+                latitude = ParseCoordinate(s: subparts[2], letter: subparts[3], kind: CoordinateKind.Latitude);
+                longitude = ParseCoordinate(s: subparts[4], letter: subparts[5], kind: CoordinateKind.Longitude);
+            }
+            catch
+            {
+                gga = default(GGA);
+                return false;
+            }
+
+            gga = new GGA(latitude, longitude);
+            return true;
+        }
+
+        private static bool SplitChecksum(string s, out string mainPart, out string checksumPart) {
+            string[] splitted = s.Split('$', '*');
+            if (splitted.Length != 3)
+            {
+                mainPart = default(string);
+                checksumPart = default(string);
+                return false;
+            }
+            mainPart = splitted[1];
+            checksumPart = splitted[2];
+            return true;
+        }
+
+        private static bool ValidateChecksum(string mainPart, string checksumPart)
+        {
+            int checksum;
+            try
+            {
+                checksum = Convert.ToInt32(checksumPart, fromBase: 16);
+            }
+            catch
+            {
+                return false;
+            }
+            if(checksum != CalcChecksum(mainPart))
+            {
+                return false;
+            }
+            return true;
+
+            int CalcChecksum(string str) => str.Aggregate(0, (acc, ch) => acc ^= ch);
+        }
+
+        private static bool SplitMainPart(string s, out string[] subparts)
+        {
+            subparts = s.Split(',');
+            return subparts.Length == GGACommaPartsNum;
+        }
+
+        private static double ParseCoordinate(string s, string letter, CoordinateKind kind)
+        {
+            if(kind == CoordinateKind.Latitude)
+            {
+                // longitude is longer by 1 char in GGA message
+                s = '0' + s;
+            }
+            var degrees = double.Parse(s.Substring(0, 3));
+            var minutes = double.Parse(s.Substring(3, s.Length - 3), CultureInfo.InvariantCulture);
+            degrees += minutes / 60;
+
+            if (letter == "S" && kind == CoordinateKind.Latitude ||
+                letter == "W" && kind == CoordinateKind.Longitude)
+            {
+                degrees = -degrees;
+            }
+
+            return degrees;
+        }
+    }
+}
--- GPSManager/MainWindow.xaml.cs
+++ GPSManager/MainWindow.xaml.cs
@@ -3,26 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
+//using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BruTile.Predefined;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.Utilities;
 
 namespace GPSManager
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            InitializeMapControl();
+        }
+
+        private void InitializeMapControl()
+        {
+            mapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            mapControl.Map.Layers.Add(CreateLayer());
+        }
+
+        public static ILayer CreateLayer()
+        {
+            var features = new Features
+            {
+                CreateSimplePoint()
+            };
+
+            var memoryProvider = new MemoryProvider(features);
+            return new MemoryLayer { Name = "Points with labels", DataSource = memoryProvider };
+        }
+
+        private static Feature CreateSimplePoint()
+        {
+            var gga = GGA.Parse("$GNGGA,033632.40,5458.46129,N,08255.44558,E,1,04,4.21,103.1,M,-38.0,M,,*61");
+            var point = SphericalMercator.FromLonLat(gga.Longitude, gga.Latitude);
+            var feature = new Feature { Geometry = point };
+            feature.Styles.Add(new LabelStyle { Text = "Default Label" });
+            return feature;
         }
     }
 }
